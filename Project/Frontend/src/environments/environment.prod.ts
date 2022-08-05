024034e5a7fe5b3590653888b057ce0798703795
Morgan Weaver <morganjweaver@gmail.com>
2022-08-05 22:01:25 +0200

    initial push

---
+++ Project/Frontend/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  network: 'goerli',
+  tokenContractAddress: '0x89791E2A68B46fB2866Dc9Ead72e87c7bC2f7352',
+  apiAddress: 'http://127.0.0.1:3000/',
+  production: true
+};
