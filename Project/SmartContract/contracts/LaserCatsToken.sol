da575eb94776a8274abb52f02cc339e6e7de0065
Thomas Gehrmann <s6thgehr@gmail.com>
2022-08-07 16:11:55 +0200

    Remove counter

--- Project/SmartContract/contracts/LaserCatsToken.sol
+++ Project/SmartContract/contracts/LaserCatsToken.sol
@@ -4,29 +4,35 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract LaserCatsToken is ERC721, ERC721URIStorage, ERC721Burnable, AccessControl {
-    using Counters for Counters.Counter;
-
+contract LaserCatsToken is
+    ERC721,
+    ERC721URIStorage,
+    ERC721Burnable,
+    AccessControl
+{
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    Counters.Counter private _tokenIdCounter;
 
     constructor() ERC721("LaserCatsToken", "LCT") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
     }
 
-    function safeMint(address to, string memory uri) public onlyRole(MINTER_ROLE) {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
+    function safeMint(
+        address to,
+        string memory uri,
+        uint256 tokenId
+    ) public onlyRole(MINTER_ROLE) {
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
 
     // The following functions are overrides required by Solidity.
 
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
         super._burn(tokenId);
     }
 
