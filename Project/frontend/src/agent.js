5d7feed5d3bc85ca54327c13c6e8780aa3594139
Hrvoje <stjepan.hrvoje@gmail.com>
2022-08-07 22:48:00 +0200

    React setup - router, semantic-ui, axios, toastify, basic agent apim basic pages

---
+++ Project/frontend/src/agent.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+axios.defaults.baseURL = process.env.API_URL;
+
+axios.interceptors.response.use(undefined, error => {
+    
+  // catch some server errors
+  if (error.message === 'Network Error' && !error.response) {
+      toast.error('Network error - make sure API works');
+  }
+
+  const { status } = error.response;
+
+  if (status === 404) {
+      history.push('/notfound');
+  }
+
+  if (status === 500) {
+      toast.error('Server error - check the terminal for more info!');
+  }
+
+  throw error.response;
+});
+
+const responseBody = (response) => response.data;
+
+const Contract = {
+  tokenBalance: (address) => axios.get(`/contract/token-balance/${address}`).then(responseBody),
+}
+
+const File = {
+  
+}
+
+export default {
+  Contract
+}
--- Project/frontend/src/components/App.jsx
+++ Project/frontend/src/components/App.jsx
@@ -1,10 +1,22 @@
-import Header from "./Header";
+import Header from "./shared/Header";
+import NotFound from "./shared/NotFound";
+import Home from "./pages/Home"
+import Details from "./pages/Details"
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { Fragment } from "react";
 
 function App() {
   return (
-    <div>
+    <Fragment>
       <Header />
-    </div>
+      <ToastContainer position="bottom-right" />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="details" element={<Details />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Fragment>
   );
 }
 
